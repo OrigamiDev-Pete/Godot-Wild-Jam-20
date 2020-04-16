2e4b88e090d49cd41bd3adddbb9572198f34f1b5
greenmoggle <55121683+greenmoggle@users.noreply.github.com>
2020-04-16 14:32:29 +1000

    Update Game.gd

--- Godot Wild Jam 20/Scripts/Game.gd
+++ Godot Wild Jam 20/Scripts/Game.gd
@@ -5,13 +5,15 @@ var time_now: int
 
 
 func _ready() -> void:
-	time_start = OS.get_unix_time()
+	time_start = OS.get_system_time_msecs()
 
 func _process(_delta: float) -> void:
-	time_now = OS.get_unix_time()
-	var elapsed = time_now - time_start
-	var minutes = elapsed / 60
-	var seconds = elapsed % 60
-	var time = "%02d:%02d" % [minutes, seconds]
+	time_now = OS.get_system_time_msecs()
+	var elapsedmsecs = time_now - time_start
+	var elapsedsecs = elapsedmsecs/1000
+	var minutes = elapsedsecs / 60
+	var seconds = elapsedsecs % 60
+	var milliseconds = elapsedmsecs % 1000
+	var time = "%02d:%02d:%02d" % [minutes, seconds, milliseconds]
 	
 	$UI/CanvasLayer/Timer.text = time
